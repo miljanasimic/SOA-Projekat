a45ab7be5d089ee6bbec5e33a39f376aadd50d55
unknown <pesic.uros@hotmail.com>
2022-06-28 01:21:14 +0200

    csproj fixed

--- Projekat/DatastreamingApp/index.js
+++ Projekat/DatastreamingApp/index.js
@@ -3,16 +3,28 @@ const { parse } = require('csv-parse');
 const axios = require('axios').default;
 
 function readData(firstLine, lastLine) {
+  let records = [];
     fs.createReadStream('./lapTimes.csv', { encoding : 'utf-8' })
-        .pipe(parse({ delimiter : ',', from_line: firstLine, to_line:lastLine, columns: true }))
+        .pipe(parse({ delimiter : ',', from_line: 1, columns: true }))
         .on('data', (chunk) => {
-            axios.post('http://localhost:5000/LapTimes', chunk)
+            records.push(chunk);
+            if(records.length > 50){
+              sendRequests(records.slice())
+            }
+        });
+}
+
+async function sendRequests(records){
+  records.forEach(async (chunk)=>{
+    setInterval(async () => {
+      axios.post('http://localhost:5000/LapTimes', chunk)
             .then(function (response) {
               })
               .catch(function (error) {
                 console.log(error.code);
               });
-        });
+    }, 5000);
+  });
 }
 
 const maxLine=400000;
