7e046bc6cb2960cc3b081ac72430d03bfd002b3d
unknown <pesic.uros@hotmail.com>
2022-06-28 00:54:20 +0200

    topic message

--- Projekat/DatabaseService/database_helpers/mongodb.js
+++ Projekat/DatabaseService/database_helpers/mongodb.js
@@ -2,14 +2,20 @@ const fs = require('fs');
 const { parse } = require('csv-parse');
 
 function insertData(filename, model){
-    const records = [];
+    let records = [];
     fs.createReadStream(filename, { encoding : 'utf-8' })   //TODO: encoding mozda treba da bude ascii, vidi ako pravi problem
         .pipe(parse({ delimiter : ',', from_line: 1, columns: true }))
         .on('data', chunk => {
             records.push(chunk);
+            if(records.length > 50){
+                populate(records, model);
+                records = [];
+            }
         })
         .on('end', () => {
-            populate(records, model)
+            if(records.length > 0){
+                populate(records, model);
+            }
         });
 }
 
