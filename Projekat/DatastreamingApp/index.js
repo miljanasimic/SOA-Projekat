28d363c9d0819ad3844ae4e7d84a0ae0af88c058
miljanasimic <simic.mimi@gmail.com>
2022-06-27 22:24:59 +0200

    .

--- Projekat/DatastreamingApp/index.js
+++ Projekat/DatastreamingApp/index.js
@@ -3,14 +3,21 @@ const { parse } = require('csv-parse');
 const axios = require('axios').default;
 
 function readData() {
-    const data=[]
     fs.createReadStream('./lapTimes.csv', { encoding : 'utf-8' })
         .pipe(parse({ delimiter : ',', from_line: 1, columns: true }))
-        .on('data', chunk=>{
-            data.push(chunk)
-        })
-    
-    console.log(data)
-    return data
+        .on('data', chunk=> {
+            setInterval(() => {
+                axios.post('http://gatewayapi:5000/laps', chunk)
+                .then(function (response) {
+                    console.log(response);
+                  })
+                .catch(function (error) {
+                console.log(error);
+                });
+            }, 1000);
+        });
 }
+
+readData();
+
 
