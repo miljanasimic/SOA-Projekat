e4597e2d785bf6924719d36ca1261cf1b29b0fec
unknown <pesic.uros@hotmail.com>
2022-06-29 02:37:03 +0200

    dodat try-catch za notifikacije

--- Projekat/AnalyticsService/GrpcClient/GrpcMethods.cs
+++ Projekat/AnalyticsService/GrpcClient/GrpcMethods.cs
@@ -7,10 +7,17 @@ namespace GrpcClient
     {
         public async static void NotifyService(MessageRequest message)
         {
-            using var channel = GrpcChannel.ForAddress("http://notificationservice:50051");
-            var client = new NotificationService.NotificationServiceClient(channel);
-            var reply = await client.NotifyAsync(message);
-            Console.WriteLine("Notification service notified...");
+            try
+            {
+                using var channel = GrpcChannel.ForAddress("http://notificationservice:50051");
+                var client = new NotificationService.NotificationServiceClient(channel);
+                var reply = await client.NotifyAsync(message);
+                Console.WriteLine("Notification service notified...");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
